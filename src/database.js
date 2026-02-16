e19532b7b01beb6a8ac196052e12aa0097bec45e
Marwan <162717872+ghmaxx5@users.noreply.github.com>
2026-02-16 10:53:45 +0530

    Add PostgreSQL Pool connection to database.js

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
+const { Pool } = require("pg");
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL
     ? { rejectUnauthorized: false }
     : false
 });
-
 
 async function initDatabase() {
   const client = await pool.connect();
